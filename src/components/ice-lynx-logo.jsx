a7c5b37ed8f2e5f9f142e8fb770924794bacce5b
P Y <py@Zs-MacBook-Pro.local>
2021-02-20 23:55:35 +0200

    improved framer motion animations

--- src/components/ice-lynx-logo.jsx
+++ src/components/ice-lynx-logo.jsx
@@ -2,14 +2,9 @@ import * as React from "react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
 export function IceLynxLogo() {
-  const boxVariants = {
-    checked: { scale: 1, background: "rgba(0,100,255,0)" },
-    unchecked: { scale: 1, background: "rgba(0,100,255,0)" },
-  };
-
   const checkVariants = {
-    checked: { pathLength: 0 },
-    unchecked: { pathLength: 1 },
+    checked: { pathLength: 0, stroke: "rgb(145,205,255)" },
+    unchecked: { pathLength: 1, stroke: "rgb(145,205,255)" },
   };
 
   const [isChecked, setIsChecked] = React.useState(true);
@@ -20,41 +15,42 @@ export function IceLynxLogo() {
     <motion.div
       class="logo"
       style={{
-        width: 90,
+        width: 100,
         height: 115,
-        borderRadius: 0,
+        borderRadius: 5,
         padding: 0,
         backgroundColor: "rgba(0,100,255,0)",
       }}
-      variants={boxVariants}
-      animate={isChecked ? "checked" : "unchecked"}
-      transition={{ duration: 0.1, type: "tween" }}
+      animate={{ rotate: 360 }}
+      
       onHoverStart={() => setIsChecked(!isChecked)}
       onHoverEnd={() => setIsChecked(!isChecked)}
     >
       <svg width="90" height="115">
-        <path d="M 25 60 l 5 0 l 10 10 l -5 0 l -10 -5" fill="#b4eafa" />
-        <path d="M 65 60 l -5 0 l -10 10 l 5 0 l 10 -5" fill="#b4eafa" />
-        <path d="M 45 80 l 5 5 l -2.5 5 l -5 0 l -2.5 -5" fill="#b4eafa" />
+        <path d="M 25 60 l 5 0 l 10 10 l -5 0 l -10 -5" fill="#91cdff" />
+        <path d="M 65 60 l -5 0 l -10 10 l 5 0 l 10 -5" fill="#91cdff" />
+        <path d="M 45 80 l 5 5 l -2.5 5 l -5 0 l -2.5 -5" fill="#91cdff" />
         <path
           d="M 45 40 l -15 -5 l -15 -15 l -5 -15 l -5 -5 l 5 15 l -5 15 l 10 15 l -10 15 l -5 20 l 10 -5 l 10 40 l 5 -25 l 5 -5 l 10 10 l 10 0 l 10 -10 l 5 5 l 5 25 l 10 -40 l 10 5 l -5 -20 l -10 -15 l 10 -15 l -5 -15 l 5 -15 l -5 5 l -5 15 l -15 15 l -15 5"
-          fill="rgba(0,0,0,0.2)"
+          fill="rgba(0,0,0,0.1)"
         />
         <svg width="90" height="115" scale="1">
           <motion.path
             d="m 5 0 l 5 5 l 5 15 l 15 15 l 15 5"
             fill="transparent"
             strokeWidth="1"
-            stroke="#b4eafa"
+            stroke="rgb(145,205,255)"
             strokeLinecap="round"
             variants={checkVariants}
             style={{ pathLength: pathLength, opacity: opacity }}
+            animate={isChecked ? "checked" : "unchecked"}
+            transition={{ duration: 1 }}
           />
           <motion.path
             d="m 85 0 l -5 5 l -5 15 l -15 15 l -15 5"
             fill="transparent"
             strokeWidth="1"
-            stroke="#b4eafa"
+            stroke="rgb(145,205,255)"
             strokeLinecap="round"
             variants={checkVariants}
             style={{ pathLength: pathLength, opacity: opacity }}
@@ -63,7 +59,7 @@ export function IceLynxLogo() {
             d="M 5 0 l 5 15 l -5 15 l 10 15 l -10 15 l -5 20 l 10 -5 l 10 40 l 5 -25 l 5 -5"
             fill="transparent"
             strokeWidth="1"
-            stroke="#b4eafa"
+            stroke="rgb(145,205,255)"
             strokeLinecap="round"
             variants={checkVariants}
             style={{ pathLength: pathLength, opacity: opacity }}
@@ -72,7 +68,7 @@ export function IceLynxLogo() {
             d="M 85 0 l -5 15 l 5 15 l -10 15 l 10 15 l 5 20 l -10 -5 l -10 40 l -5 -25 l -5 -5"
             fill="transparent"
             strokeWidth="1"
-            stroke="#b4eafa"
+            stroke="rgb(145,205,255)"
             strokeLinecap="round"
             variants={checkVariants}
             style={{ pathLength: pathLength, opacity: opacity }}
@@ -81,7 +77,7 @@ export function IceLynxLogo() {
             d="M 30 85 l 10 10 l 5 0"
             fill="transparent"
             strokeWidth="1"
-            stroke="#b4eafa"
+            stroke="rgb(145,205,255)"
             strokeLinecap="round"
             variants={checkVariants}
             style={{ pathLength: pathLength, opacity: opacity }}
@@ -90,7 +86,7 @@ export function IceLynxLogo() {
             d="M 60 85 l -10 10 l -5 0"
             fill="transparent"
             strokeWidth="1"
-            stroke="#b4eafa"
+            stroke="rgb(145,205,255)"
             strokeLinecap="round"
             variants={checkVariants}
             style={{ pathLength: pathLength, opacity: opacity }}
